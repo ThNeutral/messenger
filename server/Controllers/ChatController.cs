a8d500cbbd8fa8f11d8db906b1a92ec77caaf188
ThNeutral <gunak47ua@gmail.com>
2024-10-11 11:43:57 +0300

    added /delete-chat and /delete-users-from-chat

--- server/Controllers/ChatController.cs
+++ server/Controllers/ChatController.cs
@@ -79,7 +79,7 @@ namespace server.Controllers
                 return BadRequest(errorResponse);
             }
             var (chat, errorChat) = await _chatService.GetChatByChatID(model.chat_id);
-            if (errorChat == ErrorCodes.DB_TRANSACTION_FAILED) 
+            if (errorChat == ErrorCodes.DB_TRANSACTION_FAILED)
             {
                 errorResponse.errorMessage = "Failed to connect to database";
                 return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
@@ -195,6 +195,149 @@ namespace server.Controllers
                 return StatusCode(StatusCodes.Status403Forbidden, errorResponse);
             }
             return Ok(new { user_ids = userIDs });
+        }
+        public class DeleteUsersFromChatModel
+        {
+            public ulong chat_id;
+            public ulong[] user_ids;
+        }
+        [HttpPost("delete-members-from-chat")]
+        public async Task<IActionResult> DeleteMembersFromChat([FromBody] DeleteUsersFromChatModel model)
+        {
+            var errorResponse = new ErrorResponse();
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (token.IsNullOrEmpty())
+            {
+                errorResponse.errorMessage = "'Authorization' header was not provided";
+                return Unauthorized(errorResponse);
+            }
+            if (!ModelState.IsValid)
+            {
+                errorResponse.errorMessage = "Incorrect request body. Expected non-empty fields 'chat_id' and 'user_ids'";
+                return BadRequest(errorResponse);
+            }
+            var (chat, errorChat) = await _chatService.GetChatByChatID(model.chat_id);
+            if (errorChat == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to connect to database";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            if (errorChat == ErrorCodes.FAILED_TO_FIND_GIVEN_ENTRY)
+            {
+                errorResponse.errorMessage = "Failed to find chat with given chat_id";
+                return NotFound(errorResponse);
+            }
+            var (userAdder, errorGetUserAdder) = await _userService.GetUserByToken(token);
+            if (errorGetUserAdder == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to connect to database";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            if (errorGetUserAdder == ErrorCodes.FAILED_TO_FIND_GIVEN_ENTRY)
+            {
+                errorResponse.errorMessage = "Failed to find user with given token";
+                return NotFound(errorResponse);
+            }
+            var userIDs = new List<ulong>();
+            bool doesInclude = false;
+            foreach (var ctu in chat.ChatsToUsers)
+            {
+                userIDs.Add(ctu.UserID);
+                if (ctu.UserID == userAdder.UserID)
+                {
+                    doesInclude = true;
+                }
+            }
+            if (!doesInclude)
+            {
+                errorResponse.errorMessage = "User does not have rights to delete users from given chat";
+                return StatusCode(StatusCodes.Status403Forbidden, errorResponse);
+            }
+            var (usersToDelete, errorFindUsersToDelete) = await _userService.GetUsersByIDs(userIDs.ToArray());
+            if (errorFindUsersToDelete == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to connect to database";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            if (errorFindUsersToDelete == ErrorCodes.FAILED_TO_FIND_GIVEN_ENTRY)
+            {
+                errorResponse.errorMessage = "All of provided ids are incorrect";
+                return NotFound(errorResponse);
+            }
+            if (errorFindUsersToDelete == ErrorCodes.FAILED_TO_FIND_SOME_ENTRIES)
+            {
+                errorResponse.errorMessage = "Some of provided ids are incorrect";
+                return NotFound(errorResponse);
+            }
+            var errorDeleteUsers = await _chatService.DeleteUsersFromChat(chat, usersToDelete.ToArray());
+            if (errorDeleteUsers == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to delete users";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            return Ok();
+        }
+        public class DeleteChatModel
+        {
+            public ulong chat_id;
+        }
+        [HttpPost("delete-chat")]
+        public async Task<IActionResult> DeleteChat([FromBody] DeleteChatModel model)
+        {
+            var errorResponse = new ErrorResponse();
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (token.IsNullOrEmpty())
+            {
+                errorResponse.errorMessage = "'Authorization' header was not provided";
+                return Unauthorized(errorResponse);
+            }
+            if (!ModelState.IsValid)
+            {
+                errorResponse.errorMessage = "Incorrect request body. Expected non-empty fields 'chat_id' and 'user_ids'";
+                return BadRequest(errorResponse);
+            }
+            var (chat, errorChat) = await _chatService.GetChatByChatID(model.chat_id);
+            if (errorChat == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to connect to database";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            if (errorChat == ErrorCodes.FAILED_TO_FIND_GIVEN_ENTRY)
+            {
+                errorResponse.errorMessage = "Failed to find chat with given chat_id";
+                return NotFound(errorResponse);
+            }
+            var (userAdder, errorGetUserAdder) = await _userService.GetUserByToken(token);
+            if (errorGetUserAdder == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to connect to database";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            if (errorGetUserAdder == ErrorCodes.FAILED_TO_FIND_GIVEN_ENTRY)
+            {
+                errorResponse.errorMessage = "Failed to find user with given token";
+                return NotFound(errorResponse);
+            }
+            bool doesInclude = false;
+            foreach (var ctu in chat.ChatsToUsers)
+            {
+                if (ctu.UserID == userAdder.UserID)
+                {
+                    doesInclude = true;
+                }
+            }
+            if (!doesInclude)
+            {
+                errorResponse.errorMessage = "User does not have rights to delete users from given chat";
+                return StatusCode(StatusCodes.Status403Forbidden, errorResponse);
+            }
+            var errorDeleteChat = await _chatService.DeleteChat(chat);
+            if (errorDeleteChat == ErrorCodes.DB_TRANSACTION_FAILED)
+            {
+                errorResponse.errorMessage = "Failed to delete chat";
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
+            return Ok();
         }
     }
 }
